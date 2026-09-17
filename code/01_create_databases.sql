11f6fedc9c1ea41d02e6f63a3bbbcafbd2d75801
Vartika Yadav <vartikasaritayadav@gmail.com>
2026-09-18 02:32:28 +0530

    Update 01_create_databases.sql

--- code/01_create_databases.sql
+++ code/01_create_databases.sql
@@ -1,1 +1,12 @@
+-- 01_create_databases.sql
+-- ============================================================
 
+DROP DATABASE IF EXISTS bronze;
+DROP DATABASE IF EXISTS silver;
+DROP DATABASE IF EXISTS gold;
+
+CREATE DATABASE bronze;
+CREATE DATABASE silver;
+CREATE DATABASE gold;
+
+SHOW DATABASES;
